daffd135da1ae5b69657c92c0c9d8f2910dcbb5a
vigneshherao <vigneshfornavy@gmail.com>
2024-11-22 20:30:45 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h2 className="bg-red-300">Hai</h2>;
+}
+
+export default App;
